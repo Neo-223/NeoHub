98d8b1eab5086a9cb3e67cef5d574e1ea70882d1
Nebula <111414808+Neo-223@users.noreply.github.com>
2026-01-19 16:20:46 -0700

    Create loader.lua
    create initial game check logic and add TODO list.
    
    more will be added soon.

---
+++ loader.lua
@@ -0,0 +1,16 @@
+if game.GameId == 109397169461300 then
+	print("Sniper Duels")
+
+elseif game.GameId == 1335695570 then
+	print("Ninja Legends")
+
+else
+	print("Unknown game")
+	print("GameId:", game.GameId)
+  print("Loading Universal")
+  
+end
+-- TODO:
+-- remove game support checks from existing scripts
+-- create more scripts for different games to add to this list.
+-- maybe add executor support check ( probably not... but maybe )
