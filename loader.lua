a0456fcb02c8ace3fe9d8c3c0e361977f7bfab3f
Nebula <111414808+Neo-223@users.noreply.github.com>
2026-01-19 17:32:04 -0700

    added Sniper Duels and Ninja Legends to script hub

--- loader.lua
+++ loader.lua
@@ -1,16 +1,16 @@
 if game.GameId == 109397169461300 then
-	print("Sniper Duels")
-
+	print("Loading Sniper Duels")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Neo-223/NeoHub/refs/heads/main/Sniper%20Duels.lua"))()
 elseif game.GameId == 1335695570 then
-	print("Ninja Legends")
-
+	print("Loading Ninja Legends")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Neo-223/NeoHub/refs/heads/main/Ninja%20Legends.lua"))()
 else
 	print("Unknown game")
 	print("GameId:", game.GameId)
-  print("Loading Universal")
-  
+    print("Loading Universal")
+  	print("Universal is not currently implemented")
 end
 -- TODO:
--- remove game support checks from existing scripts
 -- create more scripts for different games to add to this list.
+-- create a scripthub gui 
 -- maybe add executor support check ( probably not... but maybe )
